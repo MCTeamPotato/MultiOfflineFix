3b211164ca71025f261d0ba6c39a88fbd55ae984
TexTrue <3140846162@qq.com>
2023-06-11 08:31:22 +0800

    1.19.2

--- common/src/main/java/team/teampotato/multiofflinefix/mixin/MinecraftClientMixin.java
+++ common/src/main/java/team/teampotato/multiofflinefix/mixin/MinecraftClientMixin.java
@@ -1,22 +1,32 @@
 package team.teampotato.multiofflinefix.mixin;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Minecraft.class)
+@Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    @Inject(at = @At("HEAD"),method = "allowsMultiplayer",cancellable = true)
-    public void multiplayer(CallbackInfoReturnable<Boolean> callbackInfo) {
-        callbackInfo.setReturnValue(true);
-        callbackInfo.cancel();
+
+
+    @Inject(at = @At("HEAD"), method = "isMultiplayerEnabled", cancellable = true)
+    private void allowMultiplayer(CallbackInfoReturnable<Boolean> info) {
+        info.setReturnValue(true);
+        info.cancel();
     }
-    @Inject(at = @At("HEAD"),method = "allowsChat",cancellable = true)
-    public void chat(CallbackInfoReturnable<Boolean> callbackInfo) {
-        callbackInfo.setReturnValue(true);
-        callbackInfo.cancel();
+
+    @Inject(at = @At("HEAD"), method = "getChatRestriction", cancellable = true)
+    private void allowChat(CallbackInfoReturnable<Boolean> info) {
+        info.setReturnValue(true);
+        info.cancel();
+    }
+
+    @Unique
+    public MinecraftClient.ChatRestriction getChatRestriction() {
+        return MinecraftClient.ChatRestriction.ENABLED;
     }
 }
